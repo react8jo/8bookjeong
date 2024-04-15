e8b0376e3576a36c3cb5ed5e068a2ab5cb378896
kirnjiyun <kimjiyunee@naver.com>
2024-04-16 03:03:32 +0900

    [Chore] 폴더 구조 세팅

---
+++ src/components/common/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as S from './footer.styled';
+
+export default function Footer() {
+  return <div>Footer</div>;
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage/MainPage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
