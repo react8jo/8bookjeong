29d234e1566f5bdc9b6c4914daa105d593980813
kirnjiyun <kimjiyunee@naver.com>
2024-04-17 02:20:09 +0900

    [Feat] 프록시 서버 사용하여 api 불러오기

---
+++ src/hooks/useBookList.js
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../services/api';
+
+const fetchNewBookList = async () => {
+  const response = await api.get('/ItemList.aspx', {
+    params: {
+      QueryType: 'ItemNewAll',
+      MaxResults: 10,
+      start: 1,
+      SearchTarget: 'Book',
+      output: 'JS',
+      Version: '20131101'
+    }
+  });
+  return response.data;
+};
+export const useNewBookListQuery = () => {
+  return useQuery({
+    queryKey: ['newBookList'],
+    queryFn: fetchNewBookList
+  });
+};
