c3d78a78fecc9d64472085a5b9e92a83a3c2754c
kirnjiyun <kimjiyunee@naver.com>
2024-04-17 01:45:54 +0900

    [Chore] 설정 빼먹은 것 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { GlobalStyle } from './assets/style/globalStyle.styled';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
