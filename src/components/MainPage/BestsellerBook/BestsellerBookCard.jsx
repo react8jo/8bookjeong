fb2026f726fdf47b5a42e0c795705b52f6784f15
Youngho Park <dudgh4651@gmail.com>
2024-04-17 21:46:06 +0900

    [Modify] bestseller slide(MainPage) 제목 저자 추가

--- src/components/MainPage/BestsellerBook/BestsellerBookCard.jsx
+++ src/components/MainPage/BestsellerBook/BestsellerBookCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './BestsellerBookCard.style.css';
 // import { useNavigate } from 'react-router-dom';
 
@@ -9,17 +9,41 @@ const BestsellerBookCard = ({ book, rank, isUpComing }) => {
   //   navigate(`/books/${isbn}`);
   // };
 
+  function getSubstringBeforeWord(inputString, word) {
+    let index = inputString.indexOf(word);
+    if (index !== -1) {
+      return inputString.substring(0, index);
+    } else {
+      // 단어를 찾지 못한 경우 전체 문자열 반환
+      return inputString;
+    }
+  }
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    if (book) {
+      setTitle(getSubstringBeforeWord(book.title, ' -'));
+      setAuthor(getSubstringBeforeWord(book.author, '(지은이)'));
+    }
+  }, [book]);
+
   return (
-    <div
-      className='BestsellerBookCard'
-      style={{
-        backgroundImage: `url(${book.cover})`
-        // backgroundPosition: 'right' // 이 부분을 추가
-      }}
-      // onClick={() => goToBookDetailPage(book.isbn)}>
-    >
-      {rank && <h4 className='ranking'>{rank}</h4>}
-      <div className='overlay'>{/* <h5>{book.title}</h5> */}</div>
+    <div>
+      <div
+        className='BestsellerBookCard'
+        style={{
+          backgroundImage: `url(${book.cover})`
+          // backgroundPosition: 'right' // 이 부분을 추가
+        }}
+        // onClick={() => goToBookDetailPage(book.isbn)}>
+      >
+        {rank && <h4 className='ranking'>{rank}</h4>}
+        <div className='overlay'>{/* <h5>{book.title}</h5> */}</div>
+      </div>
+      <div className='credit-name'>{title}</div>
+      <div className='credit-character'>{author}</div>
     </div>
   );
 };
