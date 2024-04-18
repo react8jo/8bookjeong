b0919db98a18e1d8dd32ca3d0f781a0005bd383d
pistapixie <hleehslee@gmail.com>
2024-04-18 11:11:20 +0900

    [Feat] 디테일 페이지 라우트 설정

---
+++ src/components/DetailPage/DetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailPage = () => {
+  return (
+    <div>
+      <div>detail page</div>
+    </div>
+  );
+};
+
+export default DetailPage;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage/MainPage';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import DetailPage from '../components/DetailPage/DetailPage';
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
       <Route path='*' element={<NotFoundPage />} />
+      <Route path='/products/:id' element={<DetailPage />} />
     </Routes>
   );
 }
