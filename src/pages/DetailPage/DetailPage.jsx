4bfdbd0ebc709df44fa33722dbc51c8b33f829bc
pistapixie <hleehslee@gmail.com>
2024-04-19 08:13:05 +0900

    [Modify] 디테일 페이지 스타일 수정

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './detailPage.styled';
 import { useParams } from 'react-router-dom';
-import { useBookDetailsQuery } from '../../hooks/useBookDetailsQuery';
+import { useBookDetailsQuery } from '../../hooks/useBookDetails';
 
 function DetailPage() {
   const { isbn13 } = useParams();
@@ -21,25 +21,28 @@ function DetailPage() {
         <S.BookImage src={book.cover} alt={`Cover of the book ${book.title}`} />
         <S.BookInfo>
           <h1>{book.title}</h1>
+          <S.Line />
           <S.BookMeta>
-            <span>{book.author}</span>
-            <span>{book.publisher}</span>
-            <span>{book.pubDate}</span>
+            <span>작가: {book.author}</span>
+            <span>출판사: {book.publisher}</span>
+            <span>출간일: {book.pubDate}</span>
           </S.BookMeta>
-          <p>Rating: {book.customerReviewRank}</p>
-          <p>Price: {book.priceSales}</p>
+          <S.Line />
+          <p>평점: {book.customerReviewRank}</p>
+          <p>가격: {book.priceSales}</p>
+          <S.Line />
           <S.ButtonGroup>
-            <S.Button>Add to Cart</S.Button>
-            <S.Button>Buy Now</S.Button>
+            <S.Button>장바구니에 담기</S.Button>
+            <S.Button>바로 구매하기</S.Button>
           </S.ButtonGroup>
         </S.BookInfo>
       </S.BookContent>
       <S.Line />
       <div>
-        <S.SectionTitle>Categories</S.SectionTitle>
+        <S.SectionTitle>이 책이 속한 분야</S.SectionTitle>
         <S.Content>{book.categoryName}</S.Content>
         <S.Line />
-        <S.SectionTitle>Description</S.SectionTitle>
+        <S.SectionTitle>책 소개</S.SectionTitle>
         <S.Content>{book.description || 'No description available.'}</S.Content>
         <S.Line />
         <S.SectionTitle>Link</S.SectionTitle>
