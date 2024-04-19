9811adf6492560aee9314b7f49fd049b8f8272ea
kirnjiyun <kimjiyunee@naver.com>
2024-04-20 00:19:18 +0900

    이름만 cover로 바꾸고 img 태그 사용

--- src/components/FilteredPage/BookCard.jsx
+++ src/components/FilteredPage/BookCard.jsx
@@ -21,7 +21,7 @@ const BooksCard = ({ books }) => {
     <div>
       {books.map((book) => (
         <S.BookContainer key={book.itemId} onClick={() => goToBookDetailPage(book.isbn)}>
-          <S.BookImage src={book.cover} alt='표지' />
+          <S.BookCover src={book.cover} alt={book.title} />
           <S.BookInfo>
             <S.BookTitle>{book.title}</S.BookTitle>
             <S.BookCategory>{book.categoryName}</S.BookCategory>
