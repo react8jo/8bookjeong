e969c74e202e862d7e24da032caad85ed86ddb98
kirnjiyun <kimjiyunee@naver.com>
2024-04-20 20:23:13 +0900

    [feat] 광고배너누르면 해당 도서 가게 만들기

--- src/components/MainPage/AdBanner/AdBanner.jsx
+++ src/components/MainPage/AdBanner/AdBanner.jsx
@@ -6,6 +6,8 @@ import 'react-multi-carousel/lib/styles.css';
 import prideAndPrejudice from '../../../assets/images/cover-prideandprejudice.jpeg';
 import harryPotter from '../../../assets/images/cover-hp.jpeg';
 import nineteenEightyFour from '../../../assets/images/cover-1984.jpeg';
+import { useNavigate } from 'react-router-dom';
+
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
@@ -26,23 +28,32 @@ const adBanners = [
     title: '오만과편견',
     description: '"서로 다른 둘의 오해와 편견을 넘어선 사랑 이야기"',
     imageUrl: prideAndPrejudice,
-    backgroundColor: '#a61e4d'
+    backgroundColor: '#a61e4d',
+    link: '/products/8937460882'
   },
   {
     title: '해리포터',
     description: '"죽음을 먹는 자들과 맞서 싸우는 한 소년의 이야기"',
     imageUrl: harryPotter,
-    backgroundColor: '#653516'
+    backgroundColor: '#653516',
+    link: '/products/8983922575'
   },
   {
     title: '1984',
     description: '"반인권적 사회를 고발한 디스토피아 소설."',
     imageUrl: nineteenEightyFour,
-    backgroundColor: '#1d50a1'
+    backgroundColor: '#1d50a1',
+    link: '/products/8932040575'
   }
 ];
 
 export default function AdBanner() {
+  const navigate = useNavigate();
+
+  const handleBannerClick = (link) => {
+    navigate(link);
+  };
+
   return (
     <S.AdBannerLayout>
       <Carousel
@@ -52,7 +63,7 @@ export default function AdBanner() {
         autoPlay={true}
         autoPlaySpeed={10000}>
         {adBanners.map((ad, index) => (
-          <S.BannerItem key={index} backgroundColor={ad.backgroundColor}>
+          <S.BannerItem key={index} backgroundColor={ad.backgroundColor} onClick={() => handleBannerClick(ad.link)}>
             <S.BannerContent>
               <S.BannerDescription>{ad.description}</S.BannerDescription>
               <S.BannerTitle>{ad.title}</S.BannerTitle>
