c3c436655fab90086baab7aab07c48d5403f3a95
kirnjiyun <kimjiyunee@naver.com>
2024-04-20 15:05:18 +0900

    [Modify] 프라이머리컬러 ,세컨더리컬러 설정

--- src/assets/style/globalStyle.styled.js
+++ src/assets/style/globalStyle.styled.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 export const primaryColor = '#4b3620';
-
+export const secondaryColor = '#ff7737';
 export const GlobalStyle = createGlobalStyle`
   ${reset}
 `;
