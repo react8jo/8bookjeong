06f2475ca23d1e6888766bea91913cbea6dfa122
Youngho Park <dudgh4651@gmail.com>
2024-04-21 15:13:36 +0900

    [Modify] 메인페이지 슬라이드 스타일 수정 및 필터, 조회페이지 수정

--- src/components/MainPage/NewBook/NewBookCard.styled.js
+++ src/components/MainPage/NewBook/NewBookCard.styled.js
@@ -62,8 +62,85 @@ export const NewBookTag = styled.h4`
   justify-content: center;
   width: 4vw;
   height: 4vw;
+  // width: 30%;
+  // height: 30%;
   font-size: 1.3vw;
   background: rgb(255, 119, 55);
   // background: #29c552;
   // background: #F0EDD9;
+  // @media (max-width: 3000px) {
+  //   width: 4vw;
+  //   height: 4vw;
+  //   font-size: 1.3vw;
+  // }
+
+  //처음부터는 slide responsive에 맞춰서수정했으면 됐을듯하다
+  @media (max-width: 464px) {
+    width: 11vw;
+    height: 11vw;
+    font-size: 3vw;
+    top: -10px;
+    left: -10px;
+  }
+  @media (min-width: 465px) and (max-width: 800px) {
+    width: 8vw;
+    height: 8vw;
+    font-size: 2.2vw;
+    top: -10px;
+    left: -10px;
+  }
+  @media (min-width: 801px) and (max-width: 1000px) {
+    width: 6.5vw;
+    height: 6.5vw;
+    font-size: 2vw;
+    top: -10px;
+    left: -10px;
+  }
+  @media (min-width: 1001px) and (max-width: 1300px) {
+    width: 5vw;
+    height: 5vw;
+    font-size: 1.5vw;
+    top: -10px;
+    left: -10px;
+  }
+  @media (min-width: 1301px) and (max-width: 1500px) {
+    width: 5vw;
+    height: 5vw;
+    font-size: 1.5vw;
+    left: -10px;
+  }
+  @media (min-width: 1501px) and (max-width: 1800px) {
+    width: 4vw;
+    height: 4vw;
+    font-size: 1.4vw;
+    left: -10px;
+  }
+  @media (min-width: 1801px) and (max-width: 2000px) {
+    width: 3.5vw;
+    height: 3.5vw;
+    font-size: 1.2vw;
+    left: -10px;
+  }
+  @media (min-width: 2001px) and (max-width: 3000px) {
+    width: 3vw;
+    height: 3vw;
+    font-size: 1vw;
+    left: -10px;
+  }
 `;
+
+// @media (min-width: 2000px) and(max-width: 3000px) {
+//   width: 4vw;
+//   height: 4vw;
+//   font-size: 2.2vw;
+// }
+// @media (min-width: 1024px) and(max-width: 2000px) {
+//   width: 4vw;
+//   height: 4vw;
+//   font-size: 2.2vw;
+// }
+// @media (min-width: 768px) and(max-width: 1024px) {
+//   width: 5vw;
+//   height: 5vw;
+//   // font-size: 0.1vw;
+// }
